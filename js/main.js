f07cbd29d31ab4685684216f3728987691952216
k-diachun <44328925+k-diachun@users.noreply.github.com>
2018-10-20 18:48:20 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+
+    /* For Bootstrap current state on portfolio sorting */
+
+    $('ul.nav-pills li a').click(function (e) {
+        $('ul.nav-pills li.active').removeClass('active')
+        $(this).parent('li').addClass('active')
+    })
+
+/* Sticky menu */
+$(".navbar").sticky({topSpacing: 0});
+
+/* Scroll spy and scroll filter */
+    $('#main-menu').onePageNav({
+        currentClass: "active",
+        changeHash: false,
+        scrollThreshold: 0.5,
+        scrollSpeed: 750,
+        filter: "",
+        easing: "swing"	
+     });
+
+});
